4590c59e21fc489aec9d5e459dc92e3b1f075aa7
Char99s <103144686+Char99s@users.noreply.github.com>
2024-05-14 14:39:56 +0300

    Fixed playback policy

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -142,7 +142,6 @@ export async function PATCH(
       }
       const asset = await video.assets.create({
         input: values.videoUrl,
-        playback_policy: "public",
         test: false,
       });
       await db.muxData.create({
