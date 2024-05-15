507fa646ab862cac9f961e9004626e281fdb1d6b
Char99s <103144686+Char99s@users.noreply.github.com>
2024-05-15 15:42:40 +0300

    Fix AccountForm

--- app/(auth)/(routes)/account/page.tsx
+++ app/(auth)/(routes)/account/page.tsx
@@ -1,12 +1,11 @@
-import AccountForm from './account-form'
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Account() {
-  const supabase = createClient()
+  const supabase = createClient();
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
-  return <AccountForm user={user} />
+  return <div></div>;
 }
