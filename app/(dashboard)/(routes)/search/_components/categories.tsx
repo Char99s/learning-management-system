51120670ae33dd098e31c1c3339dbe5e4701e02b
Char99s <103144686+Char99s@users.noreply.github.com>
2024-05-15 16:12:16 +0300

    updated suspense

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -31,16 +31,18 @@ const iconMap: Record<Category["name"], IconType> = {
 export const Categories = ({ items }: CategoriesProps) => {
   return (
     <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
-      <Suspense>
-        {items.map((item) => (
-          <CategoryItem
-            key={item.id}
-            label={item.name}
-            icon={iconMap[item.name]}
-            value={item.id}
-          />
-        ))}
-      </Suspense>
+      {items.map((item) => (
+        <>
+          <Suspense>
+            <CategoryItem
+              key={item.id}
+              label={item.name}
+              icon={iconMap[item.name]}
+              value={item.id}
+            />
+          </Suspense>
+        </>
+      ))}
     </div>
   );
 };
